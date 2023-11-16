1019505591d3bc675aa7f0425d6752d84021fce9
Kevin Townson <ktown5422@gmail.com>
2023-11-16 17:09:47 -0600

    add search page - KT

--- app/movies/[id]/page.jsx
+++ app/movies/[id]/page.jsx
@@ -1,32 +1,53 @@
-import { getMovieDetails } from "@/app/utils/requests";
+import { getMovieDetails, getSimilarMovies } from "@/app/utils/requests";
 
 async function MovieDetailsPage({ params }) {
     const IMAGE_BASE_URL = 'https://www.themoviedb.org/t/p/w220_and_h330_face';
     const movieDetails = await getMovieDetails(params.id);
+    const similarMovies = await getSimilarMovies(params.id);
 
     return (
-        <div className="p-10">
-            <div className="flex items-center space-x-4">
-                <div className="w-1/4">
+        <div>
+            <div className="flex flex-col items-center p-24">
+                <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+
                     <img src={IMAGE_BASE_URL + movieDetails.poster_path} alt="" className="object-cover rounded-lg" />
-                </div>
-                <div className="w-3/4 p-5">
-                    <h3 className="text-2xl font-semibold mb-2">{movieDetails.title}</h3>
-                    <div className="flex space-x-2 mb-2">
-                        <p className="bg-red-500 text-white font-semibold py-2 px-4 rounded">{movieDetails.release_date}</p>
-                        <p className="bg-red-500 text-white font-semibold py-2 px-4 rounded">{movieDetails.original_language}</p>
-                        <p className="bg-red-500 text-white font-semibold py-2 px-4 rounded">{movieDetails.status}</p>
-                    </div>
+
                     <div>
-                        <p>
-                            {movieDetails.genres.map(genre => (
-                                <span className="bg-yellow-500 text-white font-semibold py-2 px-4 rounded mr-2 mb-2" key={genre.id}>
-                                    {genre.name}
-                                </span>
-                            ))}
-                        </p>
+                        <h3 className="text-2xl font-semibold mb-2">{movieDetails.title}</h3>
+                        <div className="flex space-x-2 mb-2">
+                            <p className="bg-red-500 text-white font-semibold py-2 px-4 rounded">{movieDetails.release_date}</p>
+                            <p className="bg-red-500 text-white font-semibold py-2 px-4 rounded">{movieDetails.original_language}</p>
+                            <p className="bg-red-500 text-white font-semibold py-2 px-4 rounded">{movieDetails.status}</p>
+                        </div>
+                        <div>
+                            <p>
+                                {movieDetails.genres.map(genre => (
+                                    <span className="bg-yellow-500 text-white font-semibold py-2 px-4 rounded mr-2 mb-2" key={genre.id}>
+                                        {genre.name}
+                                    </span>
+                                ))}
+                            </p>
+                        </div>
+                        <p className="pt-5">{movieDetails.overview}</p>
                     </div>
-                    <p className="pt-5">{movieDetails.overview}</p>
+                </div>
+            </div>
+            {/* Similiar Movies */}
+            <div className="flex min-h-screen flex-col items-center justify-between p-24">
+                <h2 className="flex text-2xl font-semibold">Similiar Movies</h2>
+                <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                        {similarMovies.map(movie => {
+                            return (
+                                <div>
+                                    <img src={IMAGE_BASE_URL + movie.poster_path} />
+                                    <div>
+                                        <h5>{movie.title}</h5>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
                 </div>
             </div>
         </div>
